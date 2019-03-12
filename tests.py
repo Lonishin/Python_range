f4678333a5eaca1747c364be576956934913e416
Liubov Lonishin <43217334+Lonishin@users.noreply.github.com>
2019-03-12 22:06:03 +0300

    Solve problem with AssertRaises

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from Range_iterator import Range
+from Range import Range
 from Range_generator import GenRange
 
 
@@ -15,7 +15,10 @@ class Test(unittest.TestCase):
         self.assertEqual(list(Range(-30, -5)), list(range(-30, -5)))
 
     def test_zero_step(self):
-        self.assertRaises(ValueError, list(Range(1, 2, 0)))
+        with self.assertRaises(ValueError) as e:
+            list(Range(1, 2, 0))
+            raise ValueError(
+                'This code must not be executed')
 
     def test_gen_positive_no_step(self):
         self.assertEqual(list(GenRange(3, 5)), list(range(3, 5)))
@@ -27,4 +30,11 @@ class Test(unittest.TestCase):
         self.assertEqual(list(GenRange(-30, -5)), list(range(-30, -5)))
 
     def test_gen_zero_step(self):
-        self.assertRaises(ValueError, list(GenRange(1, 2, 0)))
+        with self.assertRaises(ValueError) as e:
+            list(GenRange(1, 2, 0))
+            raise ValueError(
+                'This code must not be executed')
+
+
+if __name__ == '__main__':
+    unittest.main()
